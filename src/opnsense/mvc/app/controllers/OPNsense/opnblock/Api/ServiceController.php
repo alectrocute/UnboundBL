6dcfe9cffbc9fd8670b80aeebc4f6ddd0304a190
alec <alecsamuelarmbruster@gmail.com>
2018-08-27 09:02:22 -0700

    Add files via upload

--- src/opnsense/mvc/app/controllers/OPNsense/opnblock/Api/ServiceController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/opnblock/Api/ServiceController.php
@@ -6,30 +6,15 @@ use \OPNsense\Core\Backend;
 
 class ServiceController extends ApiControllerBase
 {
-    /**
-     * reconfigure opnblock
-     */
-    public function reloadAction()
+    public function refreshAction()
     {
         if ($this->request->isPost()) {
             $backend = new Backend();
+                // refresh backend
                 $backend->configdRun('template reload OPNsense/opnblock');
-            return array("message" => "Configuration saved! Make sure you click the 'Reload Lists' button and restart your Unbound server.");
-        }
-        return array("message" => "Something went wrong...");
-    }
-
-    /**
-     * test opnblock
-     */
-    public function testAction()
-    {
-        if ($this->request->isPost()) {
-            $backend = new Backend();
-            $bckresult = $backend->configdRun("opnblock refresh");
-            if ($bckresult !== null) {
+                // refresh and build lists for unbound (opnblock.conf)
+                $backend->configdRun("opnblock refresh");
                 return array("message" => "OPNblock's lists have been updated! Please restart your Unbound DNS server.");
-            }
         }
         return array("message" => "Something went wrong...");
     }
--- src/opnsense/mvc/app/controllers/OPNsense/opnblock/Api/SettingsController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/opnblock/Api/SettingsController.php
@@ -1,50 +1,13 @@
 <?php
-/**
- *    Copyright (C) 2015 Deciso B.V.
- *
- *    All rights reserved.
- *
- *    Redistribution and use in source and binary forms, with or without
- *    modification, are permitted provided that the following conditions are met:
- *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *    POSSIBILITY OF SUCH DAMAGE.
- *
- */
 namespace OPNsense\opnblock\Api;
 
 use \OPNsense\Base\ApiControllerBase;
 use \OPNsense\opnblock\opnblock;
 use \OPNsense\Core\Config;
-
-/**
- * Class SettingsController Handles settings related API actions for the opnblock module
- * @package OPNsense\Cron
- */
 class SettingsController extends ApiControllerBase
 {
-    /**
-     * retrieve opnblock general settings
-     * @return array general settings
-     */
     public function getAction()
     {
-        // define list of configurable settings
         $result = array();
         if ($this->request->isGet()) {
             $mdlopnblock = new opnblock();
@@ -52,20 +15,12 @@ class SettingsController extends ApiControllerBase
         }
         return $result;
     }
-
-    /**
-     * update opnblock settings
-     * @return array status
-     */
     public function setAction()
     {
         $result = array("result"=>"failed");
         if ($this->request->isPost()) {
-            // load model and update with provided data
             $mdlopnblock = new opnblock();
             $mdlopnblock->setNodes($this->request->getPost("opnblock"));
-
-            // perform validation
             $valMsgs = $mdlopnblock->performValidation();
             foreach ($valMsgs as $field => $msg) {
                 if (!array_key_exists("validations", $result)) {
@@ -73,8 +28,6 @@ class SettingsController extends ApiControllerBase
                 }
                 $result["validations"]["opnblock.".$msg->getField()] = $msg->getMessage();
             }
-
-            // serialize model to config and save
             if ($valMsgs->count() == 0) {
                 $mdlopnblock->serializeToConfig();
                 Config::getInstance()->save();
